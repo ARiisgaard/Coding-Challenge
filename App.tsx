b748ed4db7b1cd1a6699ee1d2b4fe27564d2b9d8
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-10-13 17:05:03 +0200

    Added icons to the map

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar"
 import React, { useState } from 'react';
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import { StyleSheet, Text, View } from "react-native"
+import ReactMapGL, { Marker } from "react-map-gl";
+import { StyleSheet, Text, View } from "react-native";
+import {mockData} from "./mockData.tsx";
+import { Icon } from 'react-native-elements'
 
 export default function App() {
 
@@ -12,7 +14,8 @@ export default function App() {
   height: "100vh",
   zoom: 10
 });
-
+// This state is used for keeping track of which parking spot have been clicked
+const [selectedParkingSpot, setSelectedParkingSpot] = useState(null);
   return (
     <View style={styles.container}>
     <ReactMapGL {...viewport}
@@ -22,6 +25,24 @@ export default function App() {
         setViewport(viewport);
       }}
   >
+  {mockData.map((parkingSpot) => (
+      <Marker
+      key={parkingSpot.id}
+      latitude={parkingSpot.latitude}
+      longitude={parkingSpot.longitude}
+
+
+
+>
+<Icon
+type='font-awesome-5'
+name='parking'
+color='#517fa4'
+/>
+
+
+      </Marker>
+    ))}
   </ReactMapGL>
     </View>
   )
