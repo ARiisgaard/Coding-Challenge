58cbf747ffd9982438ab2584d8157f6ed17b0aad
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-10-13 16:12:54 +0200

    Added map

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from "expo-status-bar"
-import React from "react"
+import React, { useState } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+
+  const [viewport, setViewport] = useState({
+  latitude: 55.66034382268525,
+  longitude: 12.610791490293405,
+  width: "100vw",
+  height: "100vh",
+  zoom: 10
+});
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ReactMapGL {...viewport}
+  mapboxApiAccessToken="pk.eyJ1IjoiYXJpaXNnYWFyZCIsImEiOiJjam80NnNydW4xMWRiM2xtb2t4ZzZ6Y3Z0In0.zl26mr2ggKw3ywWTcvmsnA"
+  mapStyle="mapbox://styles/ariisgaard/ckg2esi040nhv1apgiqlqlmor"
+      onViewportChange={viewport => {
+        setViewport(viewport);
+      }}
+  >
+  </ReactMapGL>
     </View>
   )
 }
